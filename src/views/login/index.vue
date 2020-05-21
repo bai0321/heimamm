1758ba4fa9d31c103e3815f6ea7d8a513b7d3c34
bai0321 <63383019+bai0321@users.noreply.github.com>
2020-05-21 15:16:32 +0800

    6-完成了手机号自定义校验骨子额

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -8,17 +8,22 @@
         <span class="sub-title">用户登录</span>
       </div>
       <!-- form表单部分 -->
-      <el-form class="login-form">
-        <el-form-item>
-          <el-input prefix-icon="el-icon-user" placeholder="请输入手机号"></el-input>
+      <el-form :model="loginForm" :rules="rules" class="login-form">
+        <el-form-item prop="phone">
+          <el-input prefix-icon="el-icon-user" placeholder="请输入手机号" v-model="loginForm.phone"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input v-model="test" prefix-icon="el-icon-lock" placeholder="请输入密码" show-password></el-input>
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginForm.password"
+            prefix-icon="el-icon-lock"
+            placeholder="请输入密码"
+            show-password
+          ></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <el-row :gutter="18">
             <el-col :span="16">
-              <el-input prefix-icon="el-icon-key" placeholder="请输入验证码"></el-input>
+              <el-input v-model="loginForm.code" prefix-icon="el-icon-key" placeholder="请输入验证码"></el-input>
             </el-col>
             <el-col :span="8">
               <img
@@ -29,8 +34,10 @@
             </el-col>
           </el-row>
         </el-form-item>
-        <el-form-item>
-          <el-checkbox>我已阅读并同意<el-link type="primary" href='http://www.baidu.com'>用户协议</el-link>和<el-link type="primary" href='http://www.baidu.com'>隐私条款</el-link></el-checkbox>
+        <el-form-item prop="isCheck">
+          <el-checkbox v-model="loginForm.isCheck"></el-checkbox>我已阅读并同意
+          <el-link type="primary" href="http://www.baidu.com">用户协议</el-link>和
+          <el-link type="primary" href="http://www.baidu.com">隐私条款</el-link>
         </el-form-item>
         <el-form-item>
           <el-button style="width:100%" type="primary">登录</el-button>
@@ -50,9 +57,59 @@
 
 <script>
 export default {
+  name: "login",
   data() {
     return {
-      test: ""
+      loginForm: {
+        //模型
+        phone: "", //手机号
+        password: "", //密码
+        code: "" ,//验证码
+        isCheck:false //是否勾选了用户协议
+      },
+      rules: {
+        //校验规则
+        phone: [
+          //是个数组，代表这个里面可以写多个校验规则
+          // { required: true, message: "必须输入手机号", trigger: "blur" },
+          // { min: 11, max: 11, message: "手机号必须是11位", trigger: "blur" }
+          {
+            validator: (rule, value, callback) => {
+              if (!value) {
+                return callback(new Error("手机号不能为空"));
+              }
+
+              //手机号的正则表达式
+              const reg = /^1[3456789][0-9]{9}$/;
+              if (!reg.test(value)) {
+                return callback(new Error("手机号不合法"));
+              }
+              callback();
+            },
+            trigger: "blur"
+          }
+        ],
+        password: [
+          { required: true, message: "必须输入密码", trigger: "blur" },
+          { min: 6, max: 12, message: "长度在 6 到 12 个字符", trigger: "blur" }
+        ],
+        code: [
+          { required: true, message: "必须输入验证码", trigger: "blur" },
+          { min: 4, max: 4, message: "长度必须是4位", trigger: "blur" }
+        ],
+        isCheck: [
+          {
+            validator: (rule, value, callback) => {
+              if (!value) {
+                return callback(new Error("必须勾选用户协议"));
+              }
+
+              callback();
+            },
+            trigger: "change"
+          }
+        ]
+      }
     };
   }
 };
